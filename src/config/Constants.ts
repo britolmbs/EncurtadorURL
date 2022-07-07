b4ac9a16a06eb5d556fdf7a07ac5eb10a5ea912a
britolmbs <britolmbs@hotmail.com>
2022-07-07 17:06:21 -0300

    projeto finalizado

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,3 +1,4 @@
 export const config = {
 API_URL: 'http://localhost:5000',
+MONGO_CONNECTION: 'mongodb+srv://britolmbs:040616lu@urlencurter.hrhkqmq.mongodb.net/?urlencurterretryWrites=true&w=majority',
 }
--- src/controller/URLController.ts
+++ src/controller/URLController.ts
@@ -1,23 +1,31 @@
-import { config } from '../config/Constants';
-import { Request, response, Response } from 'express';
-import shortId from 'shortid';
-
+import { Request, Response } from 'express'
+import shortId from 'shortid'
+import { config } from '../config/Constants'
+import { URLModel } from '../database/model/URL'
 
 export class URLController {
-    public async shorten(req: Request, res: Response): Promise<void> {
-        const { originURL } = req.body
-        const hash = shortId.generate()
-        const shortURL = `${config.API_URL}/${hash}`
-        response.json({ originURL, hash, shortURL })
-    }
+	public async shorten(req: Request, response: Response): Promise<void> {
+		const { originURL } = req.body
+		const url = await URLModel.findOne({ originURL })
+		if (url) {
+			response.json(url)
+			return
+		}
+		const hash = shortId.generate()
+		const shortURL = `${config.API_URL}/${hash}`
+		const newURL = await URLModel.create({ hash, shortURL, originURL })
+		response.json(newURL)
+	}
 
-    public async redirect(req: Request, res: Response): Promise<void> {
-const { hash } = req.params
-const url = {
-    originURL: 'https://cloud.mongodb.com/v2/62c31c4f5684f053d5cb570d#clusters',
-    hash:'',
-    shortURL:'',
-}
-response.redirect(url.originURL)
-    }
+	public async redirect(req: Request, response: Response): Promise<void> {
+		const { hash } = req.params
+		const url = await URLModel.findOne({ hash })
+
+		if (url) {
+			response.redirect(url.originURL)
+			return
+		}
+
+		response.status(400).json({ error: 'URL not found' })
+	}
 }
